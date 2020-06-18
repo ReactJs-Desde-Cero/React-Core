98087e0c43f27238732e14beb456edab97479d3c
Alexander <alexander.ayras@gmail.com>
2020-06-18 18:26:45 -0500

    Proyecto 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
-import TarjetaFruta from './Components/TarjetaFruta'
+import { render } from '@testing-library/react'
+
+class Contador extends Component {
+
+    state = {
+        video: {
+            titulo: 'Super Video',
+            likes: 0
+        }
+    }
+
+    add = () => {
+        this.setState((state) => ({
+            video: {
+                ...state.video,
+                likes: state.video.likes + 1
+            }
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.video.titulo} 🎮 </h1>
+                <button
+                    onClick={this.add}
+                >
+                    Likes: {this.state.video.likes}
+                </button>
+            </div>
+        )
+    }
+}
 
 const App = (props) => {
     return (
         <div>
-            <TarjetaFruta name='Sandia' price='1.50' />
-            <TarjetaFruta name='Naranja' price='2.50' />
-            <TarjetaFruta name='Fresa' price='3.30' />
+            <Contador />
         </div>
     )
 }
