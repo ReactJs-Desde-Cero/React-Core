a6aa3c0ac98687908664bc2dca1f52fb5a4404bd
Alexander <alexander.ayras@gmail.com>
2020-06-18 23:05:11 -0500

    Proyecto 8

--- src/App.js
+++ src/App.js
@@ -1,50 +1,29 @@
 import React, { Component } from 'react'
-import './global.css'
 
-class Hijo extends Component {
-
-    manejadorClick = () => {
-        this.props.onSaluda('Ninja en React 🔥 ')
-    }
-
-    render() {
-        return (
-            <div className='box blue'>
-                <h2>Hijo</h2>
-                <button
-                    onClick={this.manejadorClick}
-                >
-                    Saluda
-                </button>
-            </div>
-        )
-    }
+const Saludo = (props) => {
+    return (
+        <div>
+            <div>{props.saluda && <strong>{props.name}</strong>}</div>
+            {
+                props.saluda
+                    ?
+                    <h1>Hay mensajes esperandote 😱 </h1>
+                    :
+                    <h1>wops no hay mensajes 😲 </h1>
+            }
+        </div>
+    )
 }
 
-class App extends Component {
-
-    state = {
-        name: ''
-    }
-
-    manejador = (name) => {
-        this.setState((state) => ({
-            name
-        }))
-    }
-
-    render() {
-        return (
-            <div className='box red'>
-                <Hijo
-                    onSaluda={this.manejador}
-                />
-                <h1>
-                    Nombre: {this.state.name}
-                </h1>
-            </div>
-        )
-    }
+const App = (props) => {
+    return (
+        <div>
+            <Saludo
+                saluda
+                name='Alexander'
+            />
+        </div>
+    )
 }
 
 export default App
