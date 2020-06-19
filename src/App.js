02ef5c11c763b5b3ff06e233b7688939127cd895
Alexander <alexander.ayras@gmail.com>
2020-06-19 01:35:13 -0500

    Prueba 01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class Entrada extends Component {
+
+  entrada = React.createRef()
+
+  focus = () => {
+    this.entrada.current.focus()
+  }
+  blur = () => {
+    this.entrada.current.blur()
+  }
+
+  componentDidMount() {
+    this.focus()
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.entrada} />
+        <button
+          onClick={this.focus}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Focus
+        </button>
+        <button
+          onClick={this.blur}
+        >
+          Blur
+        </button>
+      </div>
+    )
+  }
 }
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React refs 🦄 </h1>
+        <Entrada />
+      </div>
+    )
+  }
+}
+
+export default App
