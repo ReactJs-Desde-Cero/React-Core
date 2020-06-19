61f7115b9810f344eb25f4af5d242fe3a137de20
Alexander <alexander.ayras@gmail.com>
2020-06-18 19:31:44 -0500

    Proyecto 5

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import React, { Component } from 'react'
 
-const styles = {
-    height: '200px',
-    background: 'gold',
-    padding: '1em',
-    boxSizing: 'border-box'
-}
-
 class App extends Component {
 
     state = {
-        x: 0,
-        y: 0
+        text: '',
+        evento: ''
     }
 
     manejador = (event) => {
 
-        this.setState({
-            x: event.clientX,
-            y: event.clientY
-        })
+        console.log(event.target.value);
 
+        this.setState({
+            text: event.target.value,
+            evento: event.type
+        })
     }
 
     render() {
         return (
-            <div
-                style={styles}
-                onMouseMove={this.manejador}
-            >
-                <div>
-                    x: {this.state.x}
-                </div>
-                <div>
-                    y: {this.state.y}
-                </div>
+            <div>
+                <input
+                    type="tex"
+                    onChange={this.manejador}
+                    onPaste={this.manejador}
+                    onCopy={this.manejador}
+                />
+
+                <h1>
+                    {this.state.text}
+                </h1>
+                <h2> {this.state.evento} </h2>
             </div>
         )
     }
