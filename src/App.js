aa8cb145f184021b343f9db5ee7193a1760fbb39
Alexander <alexander.ayras@gmail.com>
2020-06-20 12:46:16 -0500

    Prueba 05

--- src/App.js
+++ src/App.js
@@ -1,88 +1,59 @@
 import React, { Component } from 'react'
 
-class InputControlado extends Component {
+const Unicorn = () => (
+  <span role='img' aria-label='unicornio'>
+    🦄
+  </span>
+)
 
-  state = {
-    text: '',
-    color: '#E8E8E8'
-  }
+class InpuntNoControlado extends Component {
 
-  actualizar = (event) => {
-    const text = event.target.value
-    let color = 'green'
-
-    if (text.trim() === '') {
-      color = '#E8E8E8'
-    }
-
-    if (text.trim() !== '' && text.length < 5) {
-      color = 'red'
-    }
-
-    this.setState({ text, color })
-
-    // Props
-
-    this.props.onMessenger(this.props.name, text)
+  handleSubmit = () => {
 
   }
 
   render() {
-    const styles = {
-      border: `1px solid ${this.state.color}`,
-      padding: '0.3em 0.6em',
-      outline: 'none'
-    }
     return (
-      <div>
-        <input
-          type="text"
-          value={this.state.text}
-          onChange={this.actualizar}
-          style={styles}
-          placeholder={this.props.placeholder}
-        />
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label htmlFor='name'>Nombre:</label>
+          <input
+            type="text"
+            placeholder='Nombre'
+            id='name'
+          />
+        </p>
+        <p>
+          <label htmlFor='email'>Email:</label>
+          <input
+            type="text"
+            placeholder='Email'
+            id='email'
+          />
+        </p>
+        <button>
+          Enviar
+        </button>
+      </form>
     )
   }
-
 }
 
 class App extends Component {
 
-  state = {
-    name: '',
-    email: ''
-  }
-
-  actualizar = (name, text) => {
-
-    this.setState({
-      [name]: text
-    })
+  send = (data) => {
+    console.log(data);
   }
 
   render() {
     return (
       <div>
-        <h1>Inputs Controlados 🦄 </h1>
-        <InputControlado
-          onMessenger={this.actualizar}
-          name='name'
-          placeholder='Nombre Completo'
+        <h1>
+          Atributo ref <Unicorn />
+        </h1>
+        <InpuntNoControlado
+          onSend={this.send}
         />
-        <br />
-        <InputControlado
-          onMessenger={this.actualizar}
-          name='email'
-          placeholder='Nombre Completo'
-        />
-        <h2>
-          Nombre: {this.state.name}
-        </h2>
-        <h2>
-          Email: {this.state.email}
-        </h2>
       </div>
     )
   }
