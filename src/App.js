86bd0051fcada380699cdd45344fc6e94cd31a07
Alexander <alexander.ayras@gmail.com>
2020-06-22 11:27:49 -0500

    Prueba 01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        users: [],
+        cargando: true
+    }
+
+    componentDidMount() {
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users, cargando: false }))
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
+
+        if (this.state.cargando) {
+            return <h1>Cargando...</h1>
+        }
+
+        return (
+            <div>
+                <h1>Peticiones HTTP</h1>
+                <h2>{this.state.text}</h2>
+                <ul>
+                    {this.state.users.map(user => (
+                        <li key={user.id}>
+                            {user.name}
+                            <a href={`https://${user.website}`}>
+                                Website
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default App;
