c0e398d17d00e02a9b364380ba44f4601c57bfc8
Alexander <alexander.ayras@gmail.com>
2020-06-25 22:56:51 -0500

    componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -1,100 +1,83 @@
 import React, { Component } from 'react'
 
-const itemStyles = {
+class Time extends Component {
 
-  borderBottom: '1px solid #CCC',
-  marginTop: '0.4em'
-}
-
-class Item extends Component {
-
-  handleClick = () => {
-    this.props.onRemove(this.props.item)
+  state = {
+    time: 0,
+    isPlaying: true
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  tick = null
 
-    if (nextProps.item.id !== this.props.item.id) {
-      return true
+  componentDidMount() {
+    this.play()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.tick)
+  }
+
+
+
+  play = () => {
+
+    this.setState({ isPlaying: true })
+
+    this.tick = setInterval(() => {
+      this.setState(state => ({
+        time: state.time + 1
+      }))
+    }, 1000)
+  }
+
+  pause = () => {
+    this.setState({ isPlaying: false })
+    clearInterval(this.tick)
+  }
+
+  toggle = () => {
+    if (this.state.isPlaying) {
+      this.pause()
+    } else {
+      this.play()
     }
-    return false
-
   }
-
 
   render() {
 
-    const { item } = this.props
-
-    console.log('Rnder :fire ' + item.text);
+    const { time, isPlaying } = this.state
     return (
-      <div style={itemStyles}>
+      <div>
+        <h1>{time}</h1>
         <button
-          onClick={this.handleClick}
+          onClick={this.toggle}
         >
-          x
+          {isPlaying ? 'Pause' : 'Play'}
         </button>
-        <span>
-          {item.text}
-        </span>
       </div>
     )
   }
 }
-
 class App extends Component {
 
   state = {
-    list: []
+    mostrar: true
   }
 
-  agregar = (e) => {
-    e.preventDefault()
-
-    const text = e.target[0].value
-    const id = Math.random().toString(16)
-    const pendiente = { text, id }
-
-    this.setState(state => ({
-      list: [
-        ...state.list,
-        pendiente
-      ]
-    }))
-
-    e.target[0].value = ''
-
-  }
-
-  eliminar = (e) => {
-    this.setState((state) => ({
-      list: state.list.filter(item => item.id !== e.id)
-    }))
+  desmontar = () => {
+    this.setState({ mostrar: false })
   }
 
   render() {
     return (
       <div>
-        <h3>getDerivedStateFromProps</h3>
-        <form onSubmit={this.agregar}>
-          <input
-            type='text'
-            placeholder='Ingresa tu pendiente'
-          />
-          <button>
-            Agregar
-          </button>
-        </form>
-
-        <div>
-          {this.state.list.map((item) => (
-            <Item
-              key={item.id}
-              item={item}
-              onRemove={this.eliminar}
-            />
-          ))}
-        </div>
+        <h3>componentWillUnmount</h3>
+        <button
+          onClick={this.desmontar}
+        >
+          Desmontar
+        </button>
+        {this.state.mostrar && <Time />}
       </div>
     )
   }
