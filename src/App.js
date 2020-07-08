6e969bbf4895124a2ea23b93b63b2df52253e2c8
Alexander <alexander.ayras@gmail.com>
2020-07-07 22:42:24 -0500

    Hooks callback con React.memo()

--- src/App.js
+++ src/App.js
@@ -37,18 +37,27 @@ const Button = React.memo(({ callback, children }) => {
 const App = () => {
 
   const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
 
   const incrementA = useCallback(() => {
     setA(a => a + 1)
   }, [])
+
+  const incrementB = useCallback(() => {
+    setB(b => b + a)
+  }, [a])
+
   return (
     <div>
       <Header />
       <Button callback={incrementA}>
         Increment A
       </Button>
+      <Button callback={incrementB}>
+        Increment B
+      </Button>
       <h1>
-        a: {a}
+        a: {a} - b: {b}
       </h1>
     </div>
   )
