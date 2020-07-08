c3dd11a6fc59d771df97549f8c01eb71cedc3943
Alexander <alexander.ayras@gmail.com>
2020-07-08 02:22:02 -0500

    Conf ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "es6": true
+    },
+    "extends": "eslint:recomended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ]
+    }
+};
