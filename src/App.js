e09e22bfd47335445762f4e2947d1386ca143081
Alexander <alexander.ayras@gmail.com>
2020-07-10 23:04:49 -0500

    Division de Codigo con React.lazy() y React.Suspence()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import { Counter, Title, Button } from './Components/Counter'
+import React, { useState, lazy, Suspense } from 'react'
+import { MoonLoader } from 'react-spinners'
+
+const Image = lazy(() => import('./Components/Image'))
 
 const App = () => {
+
+  const [show, setShow] = useState(false);
+
+  const toggle = () => {
+    setShow(!show)
+  }
+
   return (
-    <div>
-      <Counter>
-        <Title />
-        <Title>
-          {(clicks) => (
-            <div>
-              <h1>{clicks} Clicks</h1>
-            </div>
-          )}
-        </Title>
-        <Button type='increment' />
-        <Button type='decrement' />
-      </Counter>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    }}>
+      <button onClick={toggle}>
+        {show ? 'Ocultar' : 'Mostrar'}
+      </button>
+      {show && <Suspense fallback={<MoonLoader color='orangered' />} >
+        <Image />
+      </Suspense>}
     </div>
   )
 }
-
 export default App
---
+++ src/Components/Image.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Image.css'
+
+const Image = () => {
+    return (
+        <div className='show'>
+            <img
+                src="https://images.pexels.com/photos/2853198/pexels-photo-2853198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="Imagen Increible"
+                width='100%'
+            />
+        </div>
+    )
+}
+
+export default Image
