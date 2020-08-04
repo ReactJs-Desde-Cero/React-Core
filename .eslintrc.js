acbcad9ee9debad162a56be94623cba490a998a3
Alexander <alexander.ayras@gmail.com>
2020-08-04 02:47:11 -0500

    Workshop Utilizando React Transition

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,26 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "eslint:recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: [
+    'react',
+  ],
   rules: {
-    ident: ["error", 2],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/button-has-type': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-indent': ['error', 4],
+    quotes: [2, 'single', { avoidEscape: true }],
+    semi: [2, 'never'],
   },
-};
+}
--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import React, { useState } from "react"
-import { TransitionGroup, CSSTransition } from "react-transition-group"
-import "./App.css"
+import React from 'react'
+import Carrousel from './Components/Carrousel/Carrousel'
 
-const App = () => {
-  const [clicks, setClicks] = useState(0)
+const images = [
+  'https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/1185434/pexels-photo-1185434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/333850/pexels-photo-333850.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/54320/rose-roses-flowers-red-54320.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/73873/star-clusters-rosette-nebula-star-galaxies-73873.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+]
 
-  const increment = () => {
-    setClicks(clicks + 1)
-  }
-
-  const decrement = () => {
-    setClicks(clicks - 1)
-  }
-  return (
+const App = () => (
     <div>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <div className="box">
-        <TransitionGroup>
-          <CSSTransition timeout={1000} classNames="fade" key={clicks}>
-            <div className="hola">{clicks}</div>
-          </CSSTransition>
-        </TransitionGroup>
-      </div>
+        <Carrousel images={images} />
     </div>
-  )
-}
+)
 
 export default App
