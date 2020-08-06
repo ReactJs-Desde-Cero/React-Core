6208723c87a951933159b0ea1e742b87c494dc22
Alexander <alexander.ayras@gmail.com>
2020-08-06 11:45:25 -0500

    Workshop Utilizando React Transition 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Carrousel from './Components/Carrousel/Carrousel'
 
 const images = [
-  'https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/1185434/pexels-photo-1185434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/333850/pexels-photo-333850.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/54320/rose-roses-flowers-red-54320.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-  'https://images.pexels.com/photos/73873/star-clusters-rosette-nebula-star-galaxies-73873.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/355403/pexels-photo-355403.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/1978126/pexels-photo-1978126.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/87477/winter-snow-tree-den-87477.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/890049/pexels-photo-890049.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
 ]
 
 const App = () => (
--- src/Components/Carrousel/Carrousel.js
+++ src/Components/Carrousel/Carrousel.js
@@ -4,63 +4,63 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import './Carrousel.css'
 
 const Carrousel = ({ images }) => {
-  const [active, setActive] = useState(0)
+    const [active, setActive] = useState(0)
 
-  const left = () => {
-    if (active > 0) {
-      setActive(active - 1)
-    } else {
-      setActive(images.length - 1)
+    const left = () => {
+        if (active > 0) {
+            setActive(active - 1)
+        } else {
+            setActive(images.length - 1)
+        }
     }
-  }
 
-  const right = () => {
-    if (active < images.length - 1) {
-      setActive(active + 1)
-    } else {
-      setActive(0)
+    const right = () => {
+        if (active < images.length - 1) {
+            setActive(active + 1)
+        } else {
+            setActive(0)
+        }
     }
-  }
 
-  return (
-      <div className="Carrousel">
-          <div className="Carrousel_Buttons">
-              <button
-                onClick={left}
-              >
-                  Anterior
-              </button>
-              <button
-                onClick={right}
-              >
-                  Siguiente
-              </button>
-          </div>
-          <TransitionGroup>
-              <CSSTransition
-                timeout={1000}
-                classNames="slide"
-                key={active}
-              >
-                  <img
-                    src={images[active]}
-                    alt="Images"
-                    className="Carrousel_Img"
-                  />
-              </CSSTransition>
-          </TransitionGroup>
-      </div>
-  )
+    return (
+        <div className="Carrousel">
+            <div className="Carrousel_Buttons">
+                <button
+                    onClick={left}
+                >
+                    Anterior
+                </button>
+                <button
+                    onClick={right}
+                >
+                    Siguiente
+                </button>
+            </div>
+            <TransitionGroup>
+                <CSSTransition
+                    timeout={10000}
+                    classNames="slide"
+                    key={active}
+                >
+                    <img
+                        src={images[active]}
+                        alt="Imagenes"
+                        className="Carrousel_Img"
+                    />
+                </CSSTransition>
+            </TransitionGroup>
+        </div>
+    )
 }
 
 Carrousel.defaultProps = {
-  images: [],
+    images: [],
 }
 
 Carrousel.propTypes = {
-  images: propTypes.arrayOf(
-    propTypes.string,
-  ),
+    images: propTypes.arrayOf(
+        propTypes.string,
+    ),
 }
 
 export default Carrousel
