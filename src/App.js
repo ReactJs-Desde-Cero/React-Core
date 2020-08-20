f1ae1e7c7442fdbe3c157c9e8213fbdaaf42ebc1
Alexander <alexander.ayras@gmail.com>
2020-08-20 11:26:43 -0500

    Constantes Básicas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import './style.css'
+import { colorPrincipal } from './color'
 
 const Header = styled.header`
-    background: linear-gradient(20deg, #db7093, #daa357);
+    background: ${colorPrincipal};
     text-aling: center;
     border-radius: 0.2em;
     color: #FFF;
     padding: 0.3em;
     margin: 0.3em;
     font-size: 14px;
+
+`
+
+const Subtitle = styled.h2`
+
+    color: ${colorPrincipal};
 
 `
 
@@ -19,6 +27,9 @@ const App = () => (
                 Styled Components
             </h1>
         </Header>
+        <Subtitle>
+            Ejemplo de Titulo
+        </Subtitle>
     </div>
 )
 
