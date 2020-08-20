e2db2ae4ae3b37397faa035079e1d04e777b9b1d
Alexander <alexander.ayras@gmail.com>
2020-08-20 16:34:48 -0500

    Animaciones Ejemplo #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
 
-const anima = keyframes`
-    0% {
-        background: #000;
-        transform: scale(1);
-    }
-
-    50% {
-        background: #db7093;
-        transform: scale(0.8);
-    }
-
-    100% {
-        background: #000;
-        transform: scale(1);
-    }
-`
-
 const Header = styled.header`
     background: #db7093;
     text-align: center;
@@ -26,7 +9,37 @@ const Header = styled.header`
     padding: 0.3em;
     margin: 0.3em;
     font-size: 14px;
-    animation: ${anima} 600ms ease-in-out infinite;
+`
+
+const pulse = keyframes`
+
+    0% {
+        transform: scale(1);
+        color: #000;
+        background: gray;
+    }
+
+    50%{
+        transform: scale(1.3);
+        color: #FFF;
+        background: purple;
+    }
+
+    100% {
+        transform: scale(1);
+        color: #000;
+        background: gray;
+    }
+`
+
+const Button = styled.button`
+    padding: 1em 2.5em;
+    margin: 1em;
+    background: gray;
+
+    &:hover{
+        animation: ${pulse} 1s ease-in-out infinite;
+    }
 
 `
 
@@ -37,6 +50,9 @@ const App = () => (
                 Styled Components
             </h1>
         </Header>
+        <Button>
+            Boton
+        </Button>
     </div>
 )
 
