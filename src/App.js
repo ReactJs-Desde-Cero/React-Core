4acf74afb5b678ac5823efb64371f4128ad22bd0
Alexander <alexander.ayras@gmail.com>
2020-08-20 22:33:25 -0500

    Como usar el componente Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const Hola = () => (
     <h1>Hola</h1>
@@ -13,22 +13,37 @@ const Home = () => (
     <h1>Home</h1>
 )
 
+const navStyles = {
+  display: 'flex',
+  justifyContent: 'space-around',
+}
+
+const Navegation = () => (
+    <nav style={ navStyles }>
+        <Link to={ {
+          pathname: '/',
+          search: '?ordernar=nombre',
+          hash: '#hash-otro',
+          state: {
+            name: 'ninja Pro',
+            age: 25,
+          },
+        } }
+        >
+            Home
+        </Link>
+        <Link to='/hola'>Hola </Link>
+        <Link to='/productos' replace>Productos </Link>
+    </nav>
+)
+
 const App = () => (
     <div>
         <BrowserRouter>
-            {/* <Route path='/' exact component={ () => (<h1>Home en linea</h1>) } /> */}
-            <Route path='/' exact component={ Home } />
+            <Navegation />
+            <Route path='/' exact render={ Home } />
             <Route path='/hola' render={ Hola } />
-            <Route path='/productos'>
-                {/* <Productos /> */}
-                {({ match }) => {
-                  if (!match) return null
-                  return (
-                      <Productos />
-                  )
-                }}
-            </Route>
-            {/* <Route path='/productos' sensitive component={ Productos } /> */}
+            <Route path='/productos' render={ Productos } />
         </BrowserRouter>
     </div>
 )
