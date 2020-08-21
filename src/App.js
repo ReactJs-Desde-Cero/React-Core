85c9091362fb91bdb5b7e0f31a76818c63aa6523
Alexander <alexander.ayras@gmail.com>
2020-08-20 21:44:48 -0500

    Instalacion de Router y montando primeras rutas

--- src/App.js
+++ src/App.js
@@ -1,58 +1,28 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-const Header = styled.header`
-    background: #db7093;
-    text-align: center;
-    border-radius: 0.2em;
-    color: #FFF;
-    padding: 0.3em;
-    margin: 0.3em;
-    font-size: 14px;
-`
+const Hola = () => (
+    <h1>Hola</h1>
+)
 
-const pulse = keyframes`
-
-    0% {
-        transform: scale(1);
-        color: #000;
-        background: gray;
-    }
-
-    50%{
-        transform: scale(1.3);
-        color: #FFF;
-        background: purple;
-    }
-
-    100% {
-        transform: scale(1);
-        color: #000;
-        background: gray;
-    }
-`
-
-const Button = styled.button`
-    padding: 1em 2.5em;
-    margin: 1em;
-    background: gray;
-
-    &:hover{
-        animation: ${pulse} 1s ease-in-out infinite;
-    }
-
-`
+const Productos = () => (
+    <h1>Productos</h1>
+)
 
 const App = () => (
     <div>
-        <Header>
-            <h1>
-                Styled Components
-            </h1>
-        </Header>
-        <Button>
-            Boton
-        </Button>
+        <BrowserRouter>
+            <Route
+              path='/hola'
+              component={ Hola }
+            />
+            <section>
+                <Route path='/productos' component={ Productos } />
+            </section>
+            <div>
+                <Route path='/productos' component={ Productos } />
+            </div>
+        </BrowserRouter>
     </div>
 )
 
