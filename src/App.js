b60f8ca52757f1fd05fc2196b23e37511afe818c
Alexander <alexander.ayras@gmail.com>
2020-08-21 16:07:30 -0500

    Usando el Component Redirect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {
+  BrowserRouter, Route, NavLink, Redirect,
+} from 'react-router-dom'
+import './App.css'
+
+const Navegation = () => (
+    <nav>
+        <NavLink to='/' exact activeClassName='active'>Home</NavLink>
+        <NavLink to='/login' activeClassName='active'>Login</NavLink>
+        <NavLink to='/perfil' activeClassName='active'>Perfil</NavLink>
+    </nav>
+)
 
 const Home = () => (
     <h1>Home</h1>
 )
 
-const Videos = () => (
-    <h1>Videos</h1>
-)
+const Login = ({location}) => {
+  if (location.state) {
+    return <h2>{location.state.message}</h2>
+  }
 
-const Playlist = () => (
-    <h1>
-        Playlist
-    </h1>
-)
+  return (
+      <h1>Login</h1>
+  )
+}
+
+const isAuth = false
+
+const Perfil = () => {
+  return isAuth
+    ? <h2>Bienvenido a tu Perfil</h2>
+    : <Redirect to={ {
+      pathname: '/login',
+      state: {
+        message: 'Debes hacer login para acceder a tu perfil',
+      },
+    } }
+    />
+}
 
 const App = () => (
     <BrowserRouter>
-        <Switch>
-            <Route path='/' exact render={ Home } />
-            <Route path='/videos' render={ Playlist } />
-            <Route path='/videos' render={ Videos } />
-        </Switch>
+        <Navegation />
+        <Route path='/' exact render={ Home } />
+        <Route path='/login' exact render={ Login } />
+        <Route path='/perfil' exact render={ Perfil } />
+        <Redirect from='/p' to='/perfil' />
     </BrowserRouter>
 )
 
