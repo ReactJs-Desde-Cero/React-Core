5e3365306d9e91f7c5ec280ca7d49e265f1fcd92
Alexander <alexander.ayras@gmail.com>
2020-08-25 11:15:19 -0500

    Ejemplo integrando Midlewares de terceros redux-logger

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
         "react/jsx-first-prop-new-line": 0,
         "react/jsx-closing-bracket-location": 0,
         "arrow-parens": 0,
+        "no-alert": 0,
         "jsx-a11y/no-noninteractive-element-interactions": 0,
         "jsx-a11y/click-events-have-key-events": 0,
         // "react/jsx-equals-spacing":[2, "always"],
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,44 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import rootReducer from './reducers/index'
 
-const store = createStore(rootReducer)
+// const logger = (store) => (next) => (action) => {
+//   console.log('Ha ocurrido una accion')
+//   next(action)
+// }
+
+const confirmDeleteTodo = (store) => (next) => (action) => {
+
+  if (action.type === 'DELETE_TODO') {
+    const delet = window.confirm('¿Esta seguro de Eliminar?')
+    if (delet) {
+      next(action)
+    }
+  } else {
+    next(action)
+  }
+
+  //   setTimeout(() => {
+  //     next(action)
+  //   }, 1000)
+
+  //   if (action.type !== 'ADD_TODO') {
+  //     setTimeout(() => {
+  //       store.dispatch({
+  //         type: 'ADD_TODO',
+  //         payload: {
+  //           text: 'NOtas Notas',
+  //           id: '2derd4',
+  //           checked: false
+  //         }
+  //       })
+  //     }, 2000)
+  //   }
+
+}
+
+const midleware = applyMiddleware(confirmDeleteTodo, logger)
+
+const store = createStore(rootReducer, midleware)
 
 export default store
