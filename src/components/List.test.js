24782ad8393287c35eb73a711c3aaed6137542e7
Alexander <alexander.ayras@gmail.com>
2020-08-27 02:48:13 -0500

    Realizando pruebas a Componente con props

--- src/components/List.test.js
+++ src/components/List.test.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import Enzyme, { shallow, mount, render } from 'enzyme'
+import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import List from './List'
 
-Enzyme.configure({
+configure({
   adapter: new Adapter()
 })
 
 describe('Probando componente <List />', () => {
-  test('Validar Nodos', () => {
-
+  test('Validar nodos', () => {
     const fruits = [
-      {name: 'fresa', id: 1},
-      {name: 'pera', id: 2},
-      {name: 'uva', id: 3},
-      {name: 'platano', id: 4},
+      { name: 'fresa', id: 1 },
+      { name: 'manzana', id: 2 },
+      { name: 'naranja', id: 3 },
+      { name: 'mango', id: 4 },
     ]
 
     const wrapper = shallow(<List title='Frutas' list={fruits} />)
 
     // Validar si existe nodo
     expect(wrapper.find('h1').exists()).toBe(true)
+
     // Validar si tiene clase css
     expect(wrapper.find('h1').hasClass('big')).toBe(true)
 
@@ -35,6 +35,34 @@ describe('Probando componente <List />', () => {
 
     // Validar html
     expect(wrapper.find('ul').childAt(2).html()).toBe('<li>naranja</li>')
+  })
 
+  test('Validar actualizaciones en props', () => {
+    const fruits = [
+      { name: 'fresa', id: 1 },
+      { name: 'manzana', id: 2 },
+      { name: 'naranja', id: 3 },
+      { name: 'mango', id: 4 }
+    ]
+
+    const wrapper = shallow(<List title='Frutas' list={fruits} />)
+
+    // Validar cantidad de elementos li
+    expect(wrapper.find('li').length).toBe(4)
+
+    wrapper.setProps({
+      list: [
+        { name: 'kiwi', id: 5 }
+      ]
+    })
+
+    // Validar cantidad de elementos li
+    expect(wrapper.find('li').length).toBe(1)
+
+    wrapper.setProps({
+      title: 'Super Frutas'
+    })
+
+    expect(wrapper.find('.big').text()).toBe('Super Frutas')
   })
 })
