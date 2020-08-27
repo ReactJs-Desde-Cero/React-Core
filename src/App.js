e1f2d404a39bd5b06b6e644b155c2ca10688e30c
Alexander <alexander.ayras@gmail.com>
2020-08-27 01:56:18 -0500

    Formas de Renderizar Componente con Enzyme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   return (
       <div>
           <h1>Introduccion a Testing</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta voluptas voluptate repellat quod facilis! Ea repudiandae eos in recusandae ipsam rem eligendi nihil! Incidunt suscipit nesciunt obcaecati nihil dolorem minus!</p>
+          <h2>Otro marcado</h2>
       </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,19 @@
-import Enzyme from 'enzyme'
+import React from 'react'
+import Enzyme, { shallow, mount, render } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
 
 Enzyme.configure({
   adapter: new Adapter()
+})
+
+describe('Probando componente <App />', () => {
+  test('Probando renderizado de componente', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.find('h1').html()).toBe('<h1>Introduccion a Testing</h1>')
+    expect(wrapper.find('h1')).toHaveLength(1)
+
+    // expect(wrapper.html()).toBe('<div><h1>Introduccion a Testing</h1></div>')
+  })
 })
